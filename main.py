57968821fb37e0e8b8eb30cffc8f9103f6b4173f
danchoi1998 <danielsomangchoi@gmail.com>
2025-05-08 17:23:24 -0400

    bookbot taking arguments for path to books

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count, get_character_count, sort_dict
+import sys
 
 def get_book_text(book):
     with open(book) as f:
@@ -8,13 +9,17 @@ def get_book_text(book):
 
 def main():
 
-    sorted_char_values = sort_dict(get_character_count("books/frankenstein.txt"))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    sorted_char_values = sort_dict(get_character_count(sys.argv[1]))
 
     print(f"""
     ============ BOOKBOT ============
-    Analyzing book found at books/frankenstein.txt...
+    Analyzing book found at {sys.argv[1]}...
     ----------- Word Count ----------
-    Found {get_word_count("books/frankenstein.txt")} total words
+    Found {get_word_count(sys.argv[1])} total words
     --------- Character Count -------""")
     for kv_pair in sorted_char_values:
         print(f"{kv_pair["char"]}: {kv_pair["num"]}")
